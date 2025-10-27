9f83c87bbad84307b5395c74baf9e454a0b3140c
Alexa Loaisiga <asloaisiga@uamv.edu.ni>
2025-10-26 20:54:11 -0600

    Agrego ejercicio 1

---
+++ Pila1/Pila1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Pila1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.tbEnteros = new System.Windows.Forms.TextBox();
+            this.lbEnteros = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(407, 157);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(135, 36);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitar.Location = new System.Drawing.Point(561, 157);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(108, 36);
+            this.btnQuitar.TabIndex = 1;
+            this.btnQuitar.Text = "QUITAR";
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            this.btnQuitar.Click += new System.EventHandler(this.btnQuitar_Click);
+            // 
+            // tbEnteros
+            // 
+            this.tbEnteros.Location = new System.Drawing.Point(168, 167);
+            this.tbEnteros.Name = "tbEnteros";
+            this.tbEnteros.Size = new System.Drawing.Size(211, 22);
+            this.tbEnteros.TabIndex = 2;
+            // 
+            // lbEnteros
+            // 
+            this.lbEnteros.FormattingEnabled = true;
+            this.lbEnteros.ItemHeight = 16;
+            this.lbEnteros.Location = new System.Drawing.Point(168, 213);
+            this.lbEnteros.Name = "lbEnteros";
+            this.lbEnteros.Size = new System.Drawing.Size(501, 180);
+            this.lbEnteros.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Números enteros";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(129, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(540, 38);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PILA DE NÚMEROS ENTEROS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Contenido actual:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbEnteros);
+            this.Controls.Add(this.tbEnteros);
+            this.Controls.Add(this.btnQuitar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "PilaEj01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.TextBox tbEnteros;
+        private System.Windows.Forms.ListBox lbEnteros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Pila1/Pila1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pila1
+{
+    public partial class Form1 : Form
+    {
+        Stack<int> enteros = new Stack<int>();
+       
+        public Form1()
+
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int.TryParse(tbEnteros.Text, out int numeros);
+            enteros.Push(numeros);
+
+            Mostrar();
+            tbEnteros.Clear();
+            tbEnteros.Focus();
+        }
+
+        public void Mostrar()
+        {
+            lbEnteros.Items.Clear();
+            foreach (int numero in enteros)
+            {
+                lbEnteros.Items.Add(numero);
+            }
+            return;
+        }
+
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            if (enteros.Count > 0)
+            {
+                enteros.Pop();
+                Mostrar();
+            }
+            else if (enteros.Count == 0)
+            {
+                MessageBox.Show("YA NO HAY MAS ELEMENTOS QUE SACAR DE LA PILA", "Pila vacia");
+            }
+        }
+
+       
+    }
+}
+
