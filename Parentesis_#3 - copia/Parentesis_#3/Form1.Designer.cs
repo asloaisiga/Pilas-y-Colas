983658ad475e62479a758b63f3b6808b26b45dca
Josue David Espinoza Ramirez <jdespinozar@uamv.edu.ni>
2025-10-27 09:08:31 -0600

    Add files via upload
    Subi ejercicio definitivo parentesis

---
+++ Parentesis_#3 - copia/Parentesis_#3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Parentesis__3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.Btn_verificar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(364, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Balance de parentesis";
+            // 
+            // Btn_verificar
+            // 
+            this.Btn_verificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_verificar.Location = new System.Drawing.Point(302, 222);
+            this.Btn_verificar.Name = "Btn_verificar";
+            this.Btn_verificar.Size = new System.Drawing.Size(101, 42);
+            this.Btn_verificar.TabIndex = 2;
+            this.Btn_verificar.Text = "verificar";
+            this.Btn_verificar.UseVisualStyleBackColor = true;
+            this.Btn_verificar.Click += new System.EventHandler(this.Btn_verificar_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ingrese una expresion matematica";
+            // 
+            // Btn_salir
+            // 
+            this.Btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_salir.Location = new System.Drawing.Point(456, 222);
+            this.Btn_salir.Name = "Btn_salir";
+            this.Btn_salir.Size = new System.Drawing.Size(92, 42);
+            this.Btn_salir.TabIndex = 6;
+            this.Btn_salir.Text = "salir";
+            this.Btn_salir.UseVisualStyleBackColor = true;
+            this.Btn_salir.Click += new System.EventHandler(this.Btn_salir_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(899, 473);
+            this.Controls.Add(this.Btn_salir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Btn_verificar);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Btn_verificar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_salir;
+    }
+}
+
---
+++ Parentesis_#3 - copia/Parentesis_#3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Parentesis__3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Btn_salir_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saliste correctamente");
+            Close();
+        }
+
+        private void Btn_verificar_Click_1(object sender, EventArgs e)
+        {
+            string expresion = textBox1.Text;
+            Stack<char> pila = new Stack<char>();
+
+            foreach (char c in expresion)
+            {
+                if (c == '(')
+                {
+                    pila.Push(c); // Apila el paréntesis de apertura '('
+                }
+                else if (c == ')')
+                {
+                    if (pila.Count == 0)
+                    {
+                        // Verifica si hay más paréntesis que cierran que abren
+                        MessageBox.Show("Los paréntesis no están balanceados: Hay más paréntesis que cierran que abren.");
+                        return;
+                    }
+                    pila.Pop(); // Desapila un paréntesis de apertura cuando se encuentra un ')'
+                }
+            }
+
+            if (pila.Count == 0)
+            {
+                // Si la pila está vacía, significa que todos los paréntesis están balanceados
+                MessageBox.Show("Los paréntesis están correctamente balanceados.");
+            }
+            else
+            {
+                // Si quedan elementos en la pila, hay más paréntesis que abren que cierran
+                MessageBox.Show("Los paréntesis no están balanceados: Hay más paréntesis que abren que cierran.");
+            }
+        }
+    }
+    
+}
