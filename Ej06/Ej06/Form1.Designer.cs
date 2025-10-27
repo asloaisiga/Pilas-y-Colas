129d9358c0f2df62e168351567d44d81d28d44d1
Alexa Loaisiga <asloaisiga@uamv.edu.ni>
2025-10-26 21:47:21 -0600

    Agrego ejercicio 06

---
+++ Ej06/Ej06/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Ej06
+{
+    partial class ColaImpresión
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.lstCola = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblPendientes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COLA DE IMPRESIÓN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(65, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "🖨️ HP LASERJET INK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 159);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese nombre del documento";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(314, 208);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(94, 41);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(664, 208);
+            this.btnImprimir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(94, 41);
+            this.btnImprimir.TabIndex = 5;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // lstCola
+            // 
+            this.lstCola.FormattingEnabled = true;
+            this.lstCola.ItemHeight = 20;
+            this.lstCola.Location = new System.Drawing.Point(211, 331);
+            this.lstCola.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstCola.Name = "lstCola";
+            this.lstCola.Size = new System.Drawing.Size(545, 164);
+            this.lstCola.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(375, 294);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "DOCUMENTOS EN COLA";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(311, 156);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(447, 27);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // lblPendientes
+            // 
+            this.lblPendientes.AutoSize = true;
+            this.lblPendientes.Location = new System.Drawing.Point(556, 122);
+            this.lblPendientes.Name = "lblPendientes";
+            this.lblPendientes.Size = new System.Drawing.Size(202, 20);
+            this.lblPendientes.TabIndex = 9;
+            this.lblPendientes.Text = "Documentos Pendientes: ";
+            // 
+            // ColaImpresión
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.lblPendientes);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstCola);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ColaImpresión";
+            this.Text = "ColaImpresión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.ListBox lstCola;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblPendientes;
+    }
+}
+
---
+++ Ej06/Ej06/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ej06
+{
+    public partial class ColaImpresión : Form
+    {
+        // Cola donde voy a guardar los documentos
+        private Queue<string> colaImpresion = new Queue<string>();
+        public ColaImpresión()
+        {
+            InitializeComponent();
+            // Conecto los botones con sus funciones
+            btnAgregar.Click += btnAgregar_Click;
+            btnImprimir.Click += btnImprimir_Click;
+
+            // Muestro al iniciar el texto por defecto
+            ActualizarLista();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                MessageBox.Show("Por favor ingrese un nombre de documento.");
+                return;
+            }
+            
+
+                // Encolo el documento ingresado
+                colaImpresion.Enqueue(nombre);
+
+            // Limpio el textbox para seguir agregando rápido
+            txtNombre.Clear();
+            txtNombre.Focus();
+
+            // Actualizo la lista para que se vea el nuevo documento
+            ActualizarLista();
+
+        }
+        // Botón para simular la impresión
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            if (colaImpresion.Count == 0)
+            {
+                MessageBox.Show("No hay documentos para imprimir.");
+                return;
+            }
+
+            // Quito el primer documento de la cola
+            string documento = colaImpresion.Dequeue();
+
+            // Muestro mensaje simulando impresión
+            MessageBox.Show("Imprimiendo documento: " + documento);
+
+            // Actualizo la lista mostrando lo que queda
+            ActualizarLista();
+        }
+        // Esta función muestra los documentos en el ListBox
+        private void ActualizarLista()
+        {
+            // Actualizar título de la ventana
+            this.Text = "Cola de Impresión – Pendientes: " + colaImpresion.Count;
+
+            // Actualizar label de pendientes
+            lblPendientes.Text = "Documentos pendientes: " + colaImpresion.Count;
+
+            // Cambiar color según cantidad
+            if (colaImpresion.Count == 0)
+                lblPendientes.ForeColor = Color.Black;      // Sin pendientes
+            else if (colaImpresion.Count <= 3)
+                lblPendientes.ForeColor = Color.Green;     // Pocos
+            else if (colaImpresion.Count <= 6)
+                lblPendientes.ForeColor = Color.DarkOrange; // Moderado
+            else
+                lblPendientes.ForeColor = Color.Red;       // Muchos (alerta)
+
+            // Actualizar ListBox
+            lstCola.Items.Clear();
+
+            if (colaImpresion.Count == 0)
+            {
+                lstCola.Items.Add("No hay documentos en cola.");
+            }
+            else
+            {
+                foreach (var doc in colaImpresion)
+                {
+                    lstCola.Items.Add(doc);
+                }
+            }
+        }
+    }
+}
